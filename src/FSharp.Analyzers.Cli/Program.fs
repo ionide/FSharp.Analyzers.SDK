66369bfcfbd811fe9077342838d15c67ec98cfdb
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-12-17 12:29:33 +0100

    Add FailOnWarnings, Verbose mode, and return exit code

--- src/FSharp.Analyzers.Cli/Program.fs
+++ src/FSharp.Analyzers.Cli/Program.fs
@@ -11,9 +11,13 @@ open FSharp.Analyzers.SDK
 type Arguments =
     | Project of string
     | Analyzer of string
+    | Fail_On_Warnings of string list
+    | Verbose
 with
     interface IArgParserTemplate with
         member s.Usage = ""
+
+let mutable verbose = false
 
 let checker =
     FSharpChecker.Create(
@@ -25,23 +29,36 @@ let checker =
 let projectSystem = ProjectController(checker)
 let parser = ArgumentParser.Create<Arguments>()
 
+let printInfo text arg =
+    if verbose then
+        Console.ForegroundColor <- ConsoleColor.DarkGray
+        printf "Info : "
+        Console.ForegroundColor <- ConsoleColor.White
+        printfn text arg
+
+let printError text arg =
+    Console.ForegroundColor <- ConsoleColor.Red
+    printf "Error : "
+    printfn text arg
+    Console.ForegroundColor <- ConsoleColor.White
+
 let loadProject file =
     async {
         let! projLoading = projectSystem.LoadProject file ignore FSIRefs.TFM.NetCore (fun _ _ _ -> ())
         let filesToCheck =
             match projLoading with
             | ProjectResponse.Project proj ->
+                printInfo "Project %s loaded" file
                 proj.projectFiles
                 |> List.choose (fun file ->
                     projectSystem.GetProjectOptions file
                     |> Option.map (fun opts -> file, opts)
                 )
             | ProjectResponse.ProjectError(errorDetails) ->
-                printfn "Project loading faield: %A" errorDetails
+                printError "Project loading faield: %A" errorDetails
                 []
             | ProjectResponse.ProjectLoading(_)
             | ProjectResponse.WorkspaceLoad(_) ->
-                printfn "Shouldn't happen 2"
                 []
 
         return filesToCheck
@@ -56,7 +73,7 @@ let typeCheckFile (file,opts) =
 
     match checkAnswer with
     | FSharpCheckFileAnswer.Aborted ->
-        printfn "Checking of file %s aborted" file
+        printError "Checking of file %s aborted" file
         None
     | FSharpCheckFileAnswer.Succeeded(c) ->
         Some (file, text, parseRes, c)
@@ -74,42 +91,77 @@ let createContext (file, text: string, p: FSharpParseFileResults,c: FSharpCheckF
         Some context
     | _ -> None
 
+let runProject proj analyzers  =
+    let path =
+        Path.Combine(Environment.CurrentDirectory, proj)
+        |> Path.GetFullPath
+
+    let files =
+        loadProject path
+        |> List.choose typeCheckFile
+        |> List.choose createContext
+
+
+    files
+    |> Seq.collect (fun ctx ->
+        printInfo "Running analyzers for %s" ctx.FileName
+        analyzers |> Seq.collect (fun analyzer -> analyzer ctx)
+    )
+    |> Seq.toList
+
+let printMessages failOnWarnings (msgs: Message list) =
+    if verbose then printfn ""
+
+    msgs
+    |> Seq.iter(fun m ->
+        let color =
+            match m.Severity with
+            | Error -> ConsoleColor.Red
+            | Warning when failOnWarnings |> List.contains m.Code -> ConsoleColor.Red
+            | Warning -> ConsoleColor.DarkYellow
+            | Info -> ConsoleColor.Blue
+
+        Console.ForegroundColor <- color
+        printfn "%s(%d,%d): %s %s - %s" m.Range.FileName m.Range.StartColumn m.Range.StartLine (m.Severity.ToString()) m.Code m.Message
+        Console.ForegroundColor <- ConsoleColor.White
+    )
+    msgs
+
+let calculateExitCode failOnWarnings (msgs: Message list): int =
+    let check =
+        msgs
+        |> List.exists (fun n -> n.Severity = Error || (n.Severity = Warning && failOnWarnings |> List.contains n.Code) )
+
+    if check then -12345 else 0
 
 [<EntryPoint>]
 let main argv =
     let results = parser.ParseCommandLine argv
+    verbose <- results.Contains <@ Verbose @>
+    printInfo "Running in verbose mode%s" ""
+
     let projOpt = results.TryGetResult <@ Project @>
+    let failOnWarnings = results.GetResult(<@ Fail_On_Warnings @>, [])
+
+    printInfo "Fail On Warnings: %A" failOnWarnings
 
     let analyzersPath =
         Path.Combine(Environment.CurrentDirectory, results.GetResult (<@ Analyzer @>, "analyzers"))
         |> Path.GetFullPath
 
+    printInfo "Loading analyzers from %s" analyzersPath
+
     let analyzers = Client.loadAnalyzers analyzersPath
+    printInfo "Registed %d analyzers" analyzers.Length
 
-    match projOpt with
-    | None -> printfn "No project given. Use `--project PATH_TO_FSPROJ`. Pass path relative to current directory."
-    | Some proj ->
-        let path =
-            Path.Combine(Environment.CurrentDirectory, proj)
-            |> Path.GetFullPath
+    let results =
+        match projOpt with
+        | None ->
+            printError "No project given. Use `--project PATH_TO_FSPROJ`. Pass path relative to current directory.%s" ""
+            []
+        | Some proj ->
+            analyzers
+            |> runProject proj
+            |> printMessages failOnWarnings
 
-        let files =
-            loadProject path
-            |> List.choose typeCheckFile
-            |> List.choose createContext
-
-        let results =
-            files
-            |> Seq.collect (fun ctx ->
-                printfn "Running analyzers for %s" ctx.FileName
-                analyzers
-                |> Seq.collect (fun analyzer -> analyzer ctx)
-            )
-            |> Seq.toList
-
-        results
-        |> Seq.iter(fun m ->
-            printfn "%s(%d,%d): %s %s - %s" m.Range.FileName m.Range.StartColumn m.Range.StartLine (m.Severity.ToString()) m.Code m.Message
-
-        )
-    0 // return an integer exit code
+    calculateExitCode failOnWarnings results
