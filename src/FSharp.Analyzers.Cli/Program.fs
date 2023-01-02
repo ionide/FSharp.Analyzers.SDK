fa7487625a0645a7a5bafe67da7e3f0701a8e957
Christer van der Meeren <christer@infotech.no>
2023-01-02 17:18:46 +0100

    Format .fs files

--- src/FSharp.Analyzers.Cli/Program.fs
+++ src/FSharp.Analyzers.Cli/Program.fs
@@ -14,7 +14,7 @@ type Arguments =
     | Fail_On_Warnings of string list
     | Ignore_Files of string list
     | Verbose
-with
+
     interface IArgParserTemplate with
         member s.Usage = ""
 
@@ -22,9 +22,11 @@ let mutable verbose = false
 
 
 let createFCS () =
-    let checker = FSharpChecker.Create(projectCacheSize = 200, keepAllBackgroundResolutions = true, keepAssemblyContents = true)
+    let checker =
+        FSharpChecker.Create(projectCacheSize = 200, keepAllBackgroundResolutions = true, keepAssemblyContents = true)
     // checker.ImplicitlyStartBackgroundWork <- true
     checker
+
 let fcs = createFCS ()
 
 let parser = ArgumentParser.Create<Arguments>()
@@ -33,7 +35,7 @@ let rec mkKn (ty: System.Type) =
     if Reflection.FSharpType.IsFunction(ty) then
         let _, ran = Reflection.FSharpType.GetFunctionElements(ty)
         let f = mkKn ran
-        Reflection.FSharpValue.MakeFunction(ty, fun _ -> f)
+        Reflection.FSharpValue.MakeFunction(ty, (fun _ -> f))
     else
         box ()
 
@@ -60,94 +62,108 @@ let loadProject toolsPath projPath =
         let fcsPo = FCS.mapToFSharpProjectOptions parsed.Head parsed
 
         return fcsPo
-    } |> Async.RunSynchronously
+    }
+    |> Async.RunSynchronously
 
-let typeCheckFile (file,opts) =
+let typeCheckFile (file, opts) =
     let text = File.ReadAllText file
     let st = SourceText.ofString text
-    let (parseRes, checkAnswer) = fcs.ParseAndCheckFileInProject(file,0,st,opts) |> Async.RunSynchronously //ToDo: Validate if 0 is ok
+
+    let (parseRes, checkAnswer) =
+        fcs.ParseAndCheckFileInProject(file, 0, st, opts) |> Async.RunSynchronously //ToDo: Validate if 0 is ok
+
     match checkAnswer with
     | FSharpCheckFileAnswer.Aborted ->
         printError "Checking of file %s aborted" file
         None
-    | FSharpCheckFileAnswer.Succeeded result ->
-        Some (file, text, parseRes, result)
+    | FSharpCheckFileAnswer.Succeeded result -> Some(file, text, parseRes, result)
 
 let entityCache = EntityCache()
 
 let getAllEntities (checkResults: FSharpCheckFileResults) (publicOnly: bool) : AssemblySymbol list =
     try
         let res = [
-          yield! AssemblyContent.GetAssemblySignatureContent AssemblyContentType.Full checkResults.PartialAssemblySignature
-          let ctx = checkResults.ProjectContext
-          let assembliesByFileName =
-            ctx.GetReferencedAssemblies()
-            |> Seq.groupBy (fun asm -> asm.FileName)
-            |> Seq.map (fun (fileName, asms) -> fileName, List.ofSeq asms)
-            |> Seq.toList
-            |> List.rev // if mscorlib.dll is the first then FSC raises exception when we try to
-                        // get Content.Entities from it.
+            yield!
+                AssemblyContent.GetAssemblySignatureContent
+                    AssemblyContentType.Full
+                    checkResults.PartialAssemblySignature
+            let ctx = checkResults.ProjectContext
 
-          for fileName, signatures in assembliesByFileName do
-            let contentType = if publicOnly then AssemblyContentType.Public else AssemblyContentType.Full
-            let content = AssemblyContent.GetAssemblyContent entityCache.Locking contentType fileName signatures
-            yield! content
+            let assembliesByFileName =
+                ctx.GetReferencedAssemblies()
+                |> Seq.groupBy (fun asm -> asm.FileName)
+                |> Seq.map (fun (fileName, asms) -> fileName, List.ofSeq asms)
+                |> Seq.toList
+                |> List.rev // if mscorlib.dll is the first then FSC raises exception when we try to
+            // get Content.Entities from it.
+
+            for fileName, signatures in assembliesByFileName do
+                let contentType =
+                    if publicOnly then
+                        AssemblyContentType.Public
+                    else
+                        AssemblyContentType.Full
+
+                let content =
+                    AssemblyContent.GetAssemblyContent entityCache.Locking contentType fileName signatures
+
+                yield! content
         ]
-        res
-    with
-    | _ -> []
 
-let createContext (file, text: string, p: FSharpParseFileResults,c: FSharpCheckFileResults) =
+        res
+    with _ -> []
+
+let createContext (file, text: string, p: FSharpParseFileResults, c: FSharpCheckFileResults) =
     match c.ImplementationFile with
     | Some tast ->
-        let context : Context = {
+        let context: Context = {
             FileName = file
-            Content = text.Split([|'\n'|])
+            Content = text.Split([| '\n' |])
             ParseTree = p.ParseTree
             TypedTree = tast
             Symbols = c.PartialAssemblySignature.Entities |> Seq.toList
             GetAllEntities = getAllEntities c
         }
+
         Some context
     | _ -> None
 
 
-let runProject toolsPath proj (globs: Glob list)  =
-    let path =
-        Path.Combine(Environment.CurrentDirectory, proj)
-        |> Path.GetFullPath
+let runProject toolsPath proj (globs: Glob list) =
+    let path = Path.Combine(Environment.CurrentDirectory, proj) |> Path.GetFullPath
     let opts = loadProject toolsPath path
+
     opts.SourceFiles
     |> Array.filter (fun file ->
         match Path.GetExtension(file).ToLowerInvariant() with
         | ".fsi" ->
             printInfo $"Ignoring signature file %s{file}"
             false
-        | _ -> true
-    )
+        | _ -> true)
     |> Array.filter (fun file ->
         match globs |> List.tryFind (fun g -> g.IsMatch file) with
         | Some g ->
             printInfo $"Ignoring file %s{file} for pattern %s{g.Pattern}"
             false
-        | None -> true
-    )
-    |> Array.choose (fun f -> typeCheckFile (f,opts) |> Option.map createContext)
+        | None -> true)
+    |> Array.choose (fun f -> typeCheckFile (f, opts) |> Option.map createContext)
     |> Array.collect (fun ctx ->
         match ctx with
         | Some c ->
             printInfo "Running analyzers for %s" c.FileName
             Client.runAnalyzers c
-        | None -> failwithf "could not get context for file %s" path
-            )
-        |> Some
+        | None -> failwithf "could not get context for file %s" path)
+    |> Some
 
 let printMessages failOnWarnings (msgs: Message array) =
-    if verbose then printfn ""
-    if verbose && Array.isEmpty msgs then printfn "No messages found from the analyzer(s)"
+    if verbose then
+        printfn ""
+
+    if verbose && Array.isEmpty msgs then
+        printfn "No messages found from the analyzer(s)"
 
     msgs
-    |> Seq.iter(fun m ->
+    |> Seq.iter (fun m ->
         let color =
             match m.Severity with
             | Error -> ConsoleColor.Red
@@ -156,18 +172,29 @@ let printMessages failOnWarnings (msgs: Message array) =
             | Info -> ConsoleColor.Blue
 
         Console.ForegroundColor <- color
-        printfn "%s(%d,%d): %s %s - %s" m.Range.FileName m.Range.StartLine m.Range.StartColumn (m.Severity.ToString()) m.Code m.Message
-        Console.ForegroundColor <- ConsoleColor.White
-    )
+
+        printfn
+            "%s(%d,%d): %s %s - %s"
+            m.Range.FileName
+            m.Range.StartLine
+            m.Range.StartColumn
+            (m.Severity.ToString())
+            m.Code
+            m.Message
+
+        Console.ForegroundColor <- ConsoleColor.White)
+
     msgs
 
-let calculateExitCode failOnWarnings (msgs: Message array option): int =
+let calculateExitCode failOnWarnings (msgs: Message array option) : int =
     match msgs with
     | None -> -1
     | Some msgs ->
         let check =
             msgs
-            |> Array.exists (fun n -> n.Severity = Error || (n.Severity = Warning && failOnWarnings |> List.contains n.Code) )
+            |> Array.exists (fun n ->
+                n.Severity = Error
+                || (n.Severity = Warning && failOnWarnings |> List.contains n.Code))
 
         if check then -2 else 0
 
@@ -187,10 +214,12 @@ let main argv =
     let ignoreFiles = ignoreFiles |> List.map Glob
 
     let analyzersPath =
-        let path = results.GetResult (<@ Analyzers_Path @>, "packages/Analyzers")
-        if System.IO.Path.IsPathRooted path
-        then path
-        else Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, path))
+        let path = results.GetResult(<@ Analyzers_Path @>, "packages/Analyzers")
+
+        if System.IO.Path.IsPathRooted path then
+            path
+        else
+            Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, path))
 
     printInfo "Loading analyzers from %s" analyzersPath
 
@@ -198,6 +227,7 @@ let main argv =
     printInfo "Registered %d analyzers from %d dlls" analyzers dlls
 
     let projOpt = results.TryGetResult <@ Project @>
+
     let results =
         match projOpt with
         | None ->
@@ -205,9 +235,10 @@ let main argv =
             None
         | Some proj ->
             let project =
-                if System.IO.Path.IsPathRooted proj
-                then proj
-                else Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, proj))
+                if System.IO.Path.IsPathRooted proj then
+                    proj
+                else
+                    Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, proj))
 
             runProject toolsPath project ignoreFiles
             |> Option.map (printMessages failOnWarnings)
--- src/FSharp.Analyzers.SDK/FSharp.Analyzers.SDK.fs
+++ src/FSharp.Analyzers.SDK/FSharp.Analyzers.SDK.fs
@@ -10,34 +10,37 @@ open System.Runtime.InteropServices
 /// Marks an analyzer for scanning
 [<AttributeUsage(AttributeTargets.Method ||| AttributeTargets.Property ||| AttributeTargets.Field)>]
 type AnalyzerAttribute([<Optional; DefaultParameterValue "Analyzer">] name: string) =
-  inherit Attribute()
+    inherit Attribute()
 
-  member _.Name = name
+    member _.Name = name
 
-type Context =
-    { FileName: string
-      Content: string[]
-      ParseTree: ParsedInput
-      TypedTree: FSharpImplementationFileContents
-      Symbols: FSharpEntity list
-      GetAllEntities: bool -> AssemblySymbol list }
+type Context = {
+    FileName: string
+    Content: string[]
+    ParseTree: ParsedInput
+    TypedTree: FSharpImplementationFileContents
+    Symbols: FSharpEntity list
+    GetAllEntities: bool -> AssemblySymbol list
+}
 
-type Fix =
-    { FromRange : Text.Range
-      FromText : string
-      ToText : string }
+type Fix = {
+    FromRange: Text.Range
+    FromText: string
+    ToText: string
+}
 
 type Severity =
     | Info
     | Warning
     | Error
 
-type Message =
-    { Type: string
-      Message: string
-      Code: string
-      Severity: Severity
-      Range: Text.Range
-      Fixes: Fix list }
+type Message = {
+    Type: string
+    Message: string
+    Code: string
+    Severity: Severity
+    Range: Text.Range
+    Fixes: Fix list
+}
 
 type Analyzer = Context -> Message list
