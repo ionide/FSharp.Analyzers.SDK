03300513daa49ab12811fc0d4b1c3fba47acae5d
nojaf <florian.verdonck@outlook.com>
2023-09-08 08:41:36 +0200

    Format with Fantomas v6.2

--- samples/OptionAnalyzer/Library.fs
+++ samples/OptionAnalyzer/Library.fs
@@ -6,88 +6,84 @@ open FSharp.Compiler.Symbols
 open FSharp.Compiler.Symbols.FSharpExprPatterns
 open FSharp.Compiler.Text
 
-let rec visitExpr memberCallHandler (e:FSharpExpr) =
+let rec visitExpr memberCallHandler (e: FSharpExpr) =
     match e with
-    | AddressOf(lvalueExpr) ->
-        visitExpr memberCallHandler lvalueExpr
+    | AddressOf(lvalueExpr) -> visitExpr memberCallHandler lvalueExpr
     | AddressSet(lvalueExpr, rvalueExpr) ->
-        visitExpr memberCallHandler lvalueExpr; visitExpr memberCallHandler rvalueExpr
+        visitExpr memberCallHandler lvalueExpr
+        visitExpr memberCallHandler rvalueExpr
     | Application(funcExpr, typeArgs, argExprs) ->
-        visitExpr memberCallHandler funcExpr; visitExprs memberCallHandler argExprs
+        visitExpr memberCallHandler funcExpr
+        visitExprs memberCallHandler argExprs
     | Call(objExprOpt, memberOrFunc, typeArgs1, typeArgs2, argExprs) ->
         memberCallHandler e.Range memberOrFunc
-        visitObjArg memberCallHandler objExprOpt; visitExprs memberCallHandler argExprs
-    | Coerce(targetType, inpExpr) ->
-        visitExpr memberCallHandler inpExpr
+        visitObjArg memberCallHandler objExprOpt
+        visitExprs memberCallHandler argExprs
+    | Coerce(targetType, inpExpr) -> visitExpr memberCallHandler inpExpr
     | FastIntegerForLoop(startExpr, limitExpr, consumeExpr, isUp, debugPointAtFor, debugPointAtInOrTo) ->
-        visitExpr memberCallHandler startExpr; visitExpr memberCallHandler limitExpr; visitExpr memberCallHandler consumeExpr
-    | ILAsm(asmCode, typeArgs, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | ILFieldGet (objExprOpt, fieldType, fieldName) ->
-        visitObjArg memberCallHandler objExprOpt
-    | ILFieldSet (objExprOpt, fieldType, fieldName, valueExpr) ->
-        visitObjArg memberCallHandler objExprOpt
-    | IfThenElse (guardExpr, thenExpr, elseExpr) ->
-        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler thenExpr; visitExpr memberCallHandler elseExpr
-    | Lambda(lambdaVar, bodyExpr) ->
-        visitExpr memberCallHandler bodyExpr
+        visitExpr memberCallHandler startExpr
+        visitExpr memberCallHandler limitExpr
+        visitExpr memberCallHandler consumeExpr
+    | ILAsm(asmCode, typeArgs, argExprs) -> visitExprs memberCallHandler argExprs
+    | ILFieldGet(objExprOpt, fieldType, fieldName) -> visitObjArg memberCallHandler objExprOpt
+    | ILFieldSet(objExprOpt, fieldType, fieldName, valueExpr) -> visitObjArg memberCallHandler objExprOpt
+    | IfThenElse(guardExpr, thenExpr, elseExpr) ->
+        visitExpr memberCallHandler guardExpr
+        visitExpr memberCallHandler thenExpr
+        visitExpr memberCallHandler elseExpr
+    | Lambda(lambdaVar, bodyExpr) -> visitExpr memberCallHandler bodyExpr
     | Let((bindingVar, bindingExpr, debugPointAtBinding), bodyExpr) ->
-        visitExpr memberCallHandler bindingExpr; visitExpr memberCallHandler bodyExpr
-    | LetRec(recursiveBindings, bodyExpr) ->
-        let recursiveBindings' = recursiveBindings |> List.map (fun (mfv, expr, dp) -> (mfv, expr))
-        List.iter (snd >> visitExpr memberCallHandler) recursiveBindings'; visitExpr memberCallHandler bodyExpr
-    | NewArray(arrayType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | NewDelegate(delegateType, delegateBodyExpr) ->
-        visitExpr memberCallHandler delegateBodyExpr
-    | NewObject(objType, typeArgs, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | NewRecord(recordType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | NewTuple(tupleType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | NewUnionCase(unionType, unionCase, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | Quote(quotedExpr) ->
-        visitExpr memberCallHandler quotedExpr
-    | FSharpFieldGet(objExprOpt, recordOrClassType, fieldInfo) ->
-        visitObjArg memberCallHandler objExprOpt
-    | FSharpFieldSet(objExprOpt, recordOrClassType, fieldInfo, argExpr) ->
-        visitObjArg memberCallHandler objExprOpt; visitExpr memberCallHandler argExpr
-    | Sequential(firstExpr, secondExpr) ->
-        visitExpr memberCallHandler firstExpr; visitExpr memberCallHandler secondExpr
-    | TryFinally(bodyExpr, finalizeExpr, debugPointAtTry, debugPointAtFinally) ->
-        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler finalizeExpr
-    | TryWith(bodyExpr, _, _, catchVar, catchExpr, debugPointAtTry, debugPointAtWith) ->
-        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler catchExpr
-    | TupleGet(tupleType, tupleElemIndex, tupleExpr) ->
-        visitExpr memberCallHandler tupleExpr
-    | DecisionTree(decisionExpr, decisionTargets) ->
-        visitExpr memberCallHandler decisionExpr; List.iter (snd >> visitExpr memberCallHandler) decisionTargets
-    | DecisionTreeSuccess (decisionTargetIdx, decisionTargetExprs) ->
-        visitExprs memberCallHandler decisionTargetExprs
-    | TypeLambda(genericParam, bodyExpr) ->
+        visitExpr memberCallHandler bindingExpr
         visitExpr memberCallHandler bodyExpr
-    | TypeTest(ty, inpExpr) ->
-        visitExpr memberCallHandler inpExpr
+    | LetRec(recursiveBindings, bodyExpr) ->
+        let recursiveBindings' =
+            recursiveBindings |> List.map (fun (mfv, expr, dp) -> (mfv, expr))
+
+        List.iter (snd >> visitExpr memberCallHandler) recursiveBindings'
+        visitExpr memberCallHandler bodyExpr
+    | NewArray(arrayType, argExprs) -> visitExprs memberCallHandler argExprs
+    | NewDelegate(delegateType, delegateBodyExpr) -> visitExpr memberCallHandler delegateBodyExpr
+    | NewObject(objType, typeArgs, argExprs) -> visitExprs memberCallHandler argExprs
+    | NewRecord(recordType, argExprs) -> visitExprs memberCallHandler argExprs
+    | NewTuple(tupleType, argExprs) -> visitExprs memberCallHandler argExprs
+    | NewUnionCase(unionType, unionCase, argExprs) -> visitExprs memberCallHandler argExprs
+    | Quote(quotedExpr) -> visitExpr memberCallHandler quotedExpr
+    | FSharpFieldGet(objExprOpt, recordOrClassType, fieldInfo) -> visitObjArg memberCallHandler objExprOpt
+    | FSharpFieldSet(objExprOpt, recordOrClassType, fieldInfo, argExpr) ->
+        visitObjArg memberCallHandler objExprOpt
+        visitExpr memberCallHandler argExpr
+    | Sequential(firstExpr, secondExpr) ->
+        visitExpr memberCallHandler firstExpr
+        visitExpr memberCallHandler secondExpr
+    | TryFinally(bodyExpr, finalizeExpr, debugPointAtTry, debugPointAtFinally) ->
+        visitExpr memberCallHandler bodyExpr
+        visitExpr memberCallHandler finalizeExpr
+    | TryWith(bodyExpr, _, _, catchVar, catchExpr, debugPointAtTry, debugPointAtWith) ->
+        visitExpr memberCallHandler bodyExpr
+        visitExpr memberCallHandler catchExpr
+    | TupleGet(tupleType, tupleElemIndex, tupleExpr) -> visitExpr memberCallHandler tupleExpr
+    | DecisionTree(decisionExpr, decisionTargets) ->
+        visitExpr memberCallHandler decisionExpr
+        List.iter (snd >> visitExpr memberCallHandler) decisionTargets
+    | DecisionTreeSuccess(decisionTargetIdx, decisionTargetExprs) -> visitExprs memberCallHandler decisionTargetExprs
+    | TypeLambda(genericParam, bodyExpr) -> visitExpr memberCallHandler bodyExpr
+    | TypeTest(ty, inpExpr) -> visitExpr memberCallHandler inpExpr
     | UnionCaseSet(unionExpr, unionType, unionCase, unionCaseField, valueExpr) ->
-        visitExpr memberCallHandler unionExpr; visitExpr memberCallHandler valueExpr
-    | UnionCaseGet(unionExpr, unionType, unionCase, unionCaseField) ->
         visitExpr memberCallHandler unionExpr
-    | UnionCaseTest(unionExpr, unionType, unionCase) ->
-        visitExpr memberCallHandler unionExpr
-    | UnionCaseTag(unionExpr, unionType) ->
-        visitExpr memberCallHandler unionExpr
+        visitExpr memberCallHandler valueExpr
+    | UnionCaseGet(unionExpr, unionType, unionCase, unionCaseField) -> visitExpr memberCallHandler unionExpr
+    | UnionCaseTest(unionExpr, unionType, unionCase) -> visitExpr memberCallHandler unionExpr
+    | UnionCaseTag(unionExpr, unionType) -> visitExpr memberCallHandler unionExpr
     | ObjectExpr(objType, baseCallExpr, overrides, interfaceImplementations) ->
         visitExpr memberCallHandler baseCallExpr
         List.iter (visitObjMember memberCallHandler) overrides
         List.iter (snd >> List.iter (visitObjMember memberCallHandler)) interfaceImplementations
     | TraitCall(sourceTypes, traitName, typeArgs, typeInstantiation, argTypes, argExprs) ->
         visitExprs memberCallHandler argExprs
-    | ValueSet(valToSet, valueExpr) ->
-        visitExpr memberCallHandler valueExpr
+    | ValueSet(valToSet, valueExpr) -> visitExpr memberCallHandler valueExpr
     | WhileLoop(guardExpr, bodyExpr, debugPointAtWhile) ->
-        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler bodyExpr
+        visitExpr memberCallHandler guardExpr
+        visitExpr memberCallHandler bodyExpr
     | BaseValue baseType -> ()
     | DefaultValue defaultType -> ()
     | ThisValue thisType -> ()
@@ -95,45 +91,46 @@ let rec visitExpr memberCallHandler (e:FSharpExpr) =
     | Value(valueToGet) -> ()
     | _ -> ()
 
-and visitExprs f exprs =
-    List.iter (visitExpr f) exprs
+and visitExprs f exprs = List.iter (visitExpr f) exprs
 
-and visitObjArg f objOpt =
-    Option.iter (visitExpr f) objOpt
+and visitObjArg f objOpt = Option.iter (visitExpr f) objOpt
 
-and visitObjMember f memb =
-    visitExpr f memb.Body
+and visitObjMember f memb = visitExpr f memb.Body
 
 let rec visitDeclaration f d =
     match d with
-    | FSharpImplementationFileDeclaration.Entity (e, subDecls) ->
+    | FSharpImplementationFileDeclaration.Entity(e, subDecls) ->
         for subDecl in subDecls do
             visitDeclaration f subDecl
-    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) ->
-        visitExpr f e
-    | FSharpImplementationFileDeclaration.InitAction(e) ->
-        visitExpr f e
+    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) -> visitExpr f e
+    | FSharpImplementationFileDeclaration.InitAction(e) -> visitExpr f e
 
-let notUsed() =
-    let option : Option<int> = None
+let notUsed () =
+    let option: Option<int> = None
     option.Value
 
 [<Analyzer "OptionAnalyzer">]
-let optionValueAnalyzer : Analyzer =
+let optionValueAnalyzer: Analyzer =
     fun ctx ->
         let state = ResizeArray<range>()
+
         let handler (range: range) (m: FSharpMemberOrFunctionOrValue) =
             let name = String.Join(".", m.DeclaringEntity.Value.FullName, m.DisplayName)
+
             if name = "Microsoft.FSharp.Core.FSharpOption`1.Value" then
                 state.Add range
+
         ctx.TypedTree.Declarations |> List.iter (visitDeclaration handler)
+
         state
         |> Seq.map (fun r ->
-            { Type = "Option.Value analyzer"
-              Message = "Option.Value shouldn't be used"
-              Code = "OV001"
-              Severity = Warning
-              Range = r
-              Fixes = []}
+            {
+                Type = "Option.Value analyzer"
+                Message = "Option.Value shouldn't be used"
+                Code = "OV001"
+                Severity = Warning
+                Range = r
+                Fixes = []
+            }
         )
         |> Seq.toList
--- src/FSharp.Analyzers.Cli/Program.fs
+++ src/FSharp.Analyzers.Cli/Program.fs
@@ -14,26 +14,27 @@ type Arguments =
     | Fail_On_Warnings of string list
     | Ignore_Files of string list
     | Verbose
-with
+
     interface IArgParserTemplate with
         member s.Usage = ""
 
 let mutable verbose = false
 
-
 let createFCS () =
-    let checker = FSharpChecker.Create(projectCacheSize = 200, keepAllBackgroundResolutions = true, keepAssemblyContents = true)
+    let checker =
+        FSharpChecker.Create(projectCacheSize = 200, keepAllBackgroundResolutions = true, keepAssemblyContents = true)
     // checker.ImplicitlyStartBackgroundWork <- true
     checker
+
 let fcs = createFCS ()
 
-let parser = ArgumentParser.Create<Arguments>(errorHandler = ProcessExiter ())
+let parser = ArgumentParser.Create<Arguments>(errorHandler = ProcessExiter())
 
 let rec mkKn (ty: System.Type) =
     if Reflection.FSharpType.IsFunction(ty) then
         let _, ran = Reflection.FSharpType.GetFunctionElements(ty)
         let f = mkKn ran
-        Reflection.FSharpValue.MakeFunction(ty, fun _ -> f)
+        Reflection.FSharpValue.MakeFunction(ty, (fun _ -> f))
     else
         box ()
 
@@ -61,72 +62,88 @@ let loadProject toolsPath projPath =
         let fcsPo = FCS.mapToFSharpProjectOptions parsed.Head parsed
 
         return fcsPo
-    } |> Async.RunSynchronously
+    }
+    |> Async.RunSynchronously
 
-let typeCheckFile (file,opts) =
+let typeCheckFile (file, opts) =
     let text = File.ReadAllText file
     let st = SourceText.ofString text
-    let (parseRes, checkAnswer) = fcs.ParseAndCheckFileInProject(file,0,st,opts) |> Async.RunSynchronously //ToDo: Validate if 0 is ok
+
+    let (parseRes, checkAnswer) =
+        fcs.ParseAndCheckFileInProject(file, 0, st, opts) |> Async.RunSynchronously //ToDo: Validate if 0 is ok
+
     match checkAnswer with
     | FSharpCheckFileAnswer.Aborted ->
         printError "Checking of file %s aborted" file
         None
-    | FSharpCheckFileAnswer.Succeeded result ->
-        Some (file, text, parseRes, result)
+    | FSharpCheckFileAnswer.Succeeded result -> Some(file, text, parseRes, result)
 
 let entityCache = EntityCache()
 
 let getAllEntities (checkResults: FSharpCheckFileResults) (publicOnly: bool) : AssemblySymbol list =
     try
-        let res = [
-          yield! AssemblyContent.GetAssemblySignatureContent AssemblyContentType.Full checkResults.PartialAssemblySignature
-          let ctx = checkResults.ProjectContext
-          let assembliesByFileName =
-            ctx.GetReferencedAssemblies()
-            |> Seq.groupBy (fun asm -> asm.FileName)
-            |> Seq.map (fun (fileName, asms) -> fileName, List.ofSeq asms)
-            |> Seq.toList
-            |> List.rev // if mscorlib.dll is the first then FSC raises exception when we try to
-                        // get Content.Entities from it.
+        let res =
+            [
+                yield!
+                    AssemblyContent.GetAssemblySignatureContent
+                        AssemblyContentType.Full
+                        checkResults.PartialAssemblySignature
+                let ctx = checkResults.ProjectContext
 
-          for fileName, signatures in assembliesByFileName do
-            let contentType = if publicOnly then AssemblyContentType.Public else AssemblyContentType.Full
-            let content = AssemblyContent.GetAssemblyContent entityCache.Locking contentType fileName signatures
-            yield! content
-        ]
+                let assembliesByFileName =
+                    ctx.GetReferencedAssemblies()
+                    |> Seq.groupBy (fun asm -> asm.FileName)
+                    |> Seq.map (fun (fileName, asms) -> fileName, List.ofSeq asms)
+                    |> Seq.toList
+                    |> List.rev // if mscorlib.dll is the first then FSC raises exception when we try to
+                // get Content.Entities from it.
+
+                for fileName, signatures in assembliesByFileName do
+                    let contentType =
+                        if publicOnly then
+                            AssemblyContentType.Public
+                        else
+                            AssemblyContentType.Full
+
+                    let content =
+                        AssemblyContent.GetAssemblyContent entityCache.Locking contentType fileName signatures
+
+                    yield! content
+            ]
+
         res
-    with
-    | _ -> []
+    with _ ->
+        []
 
 let createContext
     (checkProjectResults: FSharpCheckProjectResults, allSymbolUses: FSharpSymbolUse array)
-    (file, text: string, p: FSharpParseFileResults, c: FSharpCheckFileResults) =
+    (file, text: string, p: FSharpParseFileResults, c: FSharpCheckFileResults)
+    =
     match c.ImplementationFile with
     | Some tast ->
-        let context : Context = {
-            ParseFileResults = p
-            CheckFileResults = c
-            CheckProjectResults = checkProjectResults 
-            FileName = file
-            Content = text.Split([|'\n'|])
-            TypedTree = tast
-            GetAllEntities = getAllEntities c
-            AllSymbolUses = allSymbolUses
-            SymbolUsesOfFile = allSymbolUses |> Array.filter (fun s -> s.FileName = file) 
-        }
+        let context: Context =
+            {
+                ParseFileResults = p
+                CheckFileResults = c
+                CheckProjectResults = checkProjectResults
+                FileName = file
+                Content = text.Split([| '\n' |])
+                TypedTree = tast
+                GetAllEntities = getAllEntities c
+                AllSymbolUses = allSymbolUses
+                SymbolUsesOfFile = allSymbolUses |> Array.filter (fun s -> s.FileName = file)
+            }
+
         Some context
     | _ -> None
 
-
-let runProject toolsPath proj (globs: Glob list)  =
-    let path =
-        Path.Combine(Environment.CurrentDirectory, proj)
-        |> Path.GetFullPath
+let runProject toolsPath proj (globs: Glob list) =
+    let path = Path.Combine(Environment.CurrentDirectory, proj) |> Path.GetFullPath
     let opts = loadProject toolsPath path
-    
+
     let checkProjectResults = fcs.ParseAndCheckProject(opts) |> Async.RunSynchronously
     let allSymbolUses = checkProjectResults.GetAllUsesOfAllSymbols()
-    
+
     opts.SourceFiles
     |> Array.filter (fun file ->
         match Path.GetExtension(file).ToLowerInvariant() with
@@ -142,22 +159,28 @@ let runProject toolsPath proj (globs: Glob list)  =
             false
         | None -> true
     )
-    |> Array.choose (fun f -> typeCheckFile (f, opts) |> Option.map (createContext (checkProjectResults, allSymbolUses)))
+    |> Array.choose (fun f ->
+        typeCheckFile (f, opts)
+        |> Option.map (createContext (checkProjectResults, allSymbolUses))
+    )
     |> Array.collect (fun ctx ->
         match ctx with
         | Some c ->
             printInfo "Running analyzers for %s" c.FileName
             Client.runAnalyzers c
         | None -> failwithf "could not get context for file %s" path
-            )
-        |> Some
+    )
+    |> Some
 
 let printMessages failOnWarnings (msgs: Message array) =
-    if verbose then printfn ""
-    if verbose && Array.isEmpty msgs then printfn "No messages found from the analyzer(s)"
+    if verbose then
+        printfn ""
+
+    if verbose && Array.isEmpty msgs then
+        printfn "No messages found from the analyzer(s)"
 
     msgs
-    |> Seq.iter(fun m ->
+    |> Seq.iter (fun m ->
         let color =
             match m.Severity with
             | Error -> ConsoleColor.Red
@@ -166,18 +189,31 @@ let printMessages failOnWarnings (msgs: Message array) =
             | Info -> ConsoleColor.Blue
 
         Console.ForegroundColor <- color
-        printfn "%s(%d,%d): %s %s - %s" m.Range.FileName m.Range.StartLine m.Range.StartColumn (m.Severity.ToString()) m.Code m.Message
+
+        printfn
+            "%s(%d,%d): %s %s - %s"
+            m.Range.FileName
+            m.Range.StartLine
+            m.Range.StartColumn
+            (m.Severity.ToString())
+            m.Code
+            m.Message
+
         Console.ForegroundColor <- origForegroundColor
     )
+
     msgs
 
-let calculateExitCode failOnWarnings (msgs: Message array option): int =
+let calculateExitCode failOnWarnings (msgs: Message array option) : int =
     match msgs with
     | None -> -1
     | Some msgs ->
         let check =
             msgs
-            |> Array.exists (fun n -> n.Severity = Error || (n.Severity = Warning && failOnWarnings |> List.contains n.Code) )
+            |> Array.exists (fun n ->
+                n.Severity = Error
+                || (n.Severity = Warning && failOnWarnings |> List.contains n.Code)
+            )
 
         if check then -2 else 0
 
@@ -197,10 +233,12 @@ let main argv =
     let ignoreFiles = ignoreFiles |> List.map Glob
 
     let analyzersPath =
-        let path = results.GetResult (<@ Analyzers_Path @>, "packages/Analyzers")
-        if System.IO.Path.IsPathRooted path
-        then path
-        else Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, path))
+        let path = results.GetResult(<@ Analyzers_Path @>, "packages/Analyzers")
+
+        if System.IO.Path.IsPathRooted path then
+            path
+        else
+            Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, path))
 
     printInfo "Loading analyzers from %s" analyzersPath
 
@@ -208,6 +246,7 @@ let main argv =
     printInfo "Registered %d analyzers from %d dlls" analyzers dlls
 
     let projOpt = results.TryGetResult <@ Project @>
+
     let results =
         match projOpt with
         | None ->
@@ -215,9 +254,10 @@ let main argv =
             None
         | Some proj ->
             let project =
-                if System.IO.Path.IsPathRooted proj
-                then proj
-                else Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, proj))
+                if System.IO.Path.IsPathRooted proj then
+                    proj
+                else
+                    Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, proj))
 
             runProject toolsPath project ignoreFiles
             |> Option.map (printMessages failOnWarnings)
