07a40056799e46cfeb2852ad1fec3030147b467b
nojaf <florian.verdonck@outlook.com>
2023-09-11 15:01:15 +0200

    Use build script

---
+++ build.fsx
@@ -0,0 +1,32 @@
+#r "nuget: Fun.Build, 0.5.2"
+
+open Fun.Build
+
+let restoreStage =
+    stage "restore" {
+        run "dotnet tool restore"
+        run "dotnet restore --locked-mode"
+    }
+
+let buildStage =
+    stage "build" { run "dotnet build -c Release --no-restore -maxCpuCount" }
+
+pipeline "Build" {
+    restoreStage
+    stage "lint" { run "dotnet fantomas . --check" }
+    stage "build" { run "dotnet build -c Release --no-restore -maxCpuCount" }
+    stage "test" { run "dotnet test -c Release --no-build" }
+    stage "sample" {
+        run
+            "dotnet run --project src/FSharp.Analyzers.Cli/FSharp.Analyzers.Cli.fsproj -- --project ./samples/OptionAnalyzer/OptionAnalyzer.fsproj --analyzers-path ./samples/OptionAnalyzer/bin/Release --verbose"
+    }
+    runIfOnlySpecified false
+}
+
+pipeline "ReleaseBuild" {
+    restoreStage
+    buildStage
+    runIfOnlySpecified true
+}
+
+tryPrintPipelineCommandHelp ()
