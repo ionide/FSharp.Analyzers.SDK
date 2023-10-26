096cf3f1065252f532154dd400efd73a46e82560
dawe <dawedawe@posteo.de>
2023-10-26 19:03:36 +0200

    add information for solutions

--- docs/content/Getting Started.fsx
+++ docs/content/Getting Started.fsx
@@ -9,11 +9,11 @@ index: 1
 
 ## Premise
 
-We assume the analyzers to be used are distributed as a nuget package.
+We assume the analyzers you want to use are distributed as a nuget package.
 
 ## Using analyzers in a single project
 
-First, we need to add the `fsharp-analyzers` dotnet tool to the tool-manifest.
+First, we need to add the [fsharp-analyzers](https://www.nuget.org/packages/fsharp-analyzers) dotnet tool to the tool-manifest.
 ```shell
 dotnet tool install fsharp-analyzers
 ```
@@ -27,7 +27,7 @@ Next, add the `PackageReference` pointing to your favorite analyzers to the `.fs
 </PackageReference>
 ```
 
-Finally, add a custom MSBuild target for easy invocation of the analyzer:
+Finally, add a custom MSBuild target to the `.fsproj` file for easy invocation of the analyzer:
 
 ```xml
 <Target
@@ -46,12 +46,83 @@ Finally, add a custom MSBuild target for easy invocation of the analyzer:
 
 You may need to adjust the `Command` to be compatible with your specific analyzer. Think about how you want warnings to be treated.
 
-At last, we can run the analyzer from the project folder:
+At last, you can run the analyzer from the project folder:
 
 ```shell
 dotnet msbuild /t:AnalyzeProject
 ```
+
+## Using analyzers in a solution
+
+First, we need to add the [fsharp-analyzers](https://www.nuget.org/packages/fsharp-analyzers) dotnet tool to the tool-manifest.
+```shell
+dotnet tool install fsharp-analyzers
+```
+
+Next, add the `PackageReference` pointing to your favorite analyzers to the [Directory.Build.props](https://learn.microsoft.com/en-us/visualstudio/msbuild/customize-by-directory?view=vs-2022) file:
+
+```xml
+<ItemGroup>
+    <PackageReference Include="G-Research.FSharp.Analyzers" Version="0.1.6">
+    <PrivateAssets>all</PrivateAssets>
+    <IncludeAssets>build</IncludeAssets>
+    </PackageReference>
+</ItemGroup>
+```
+
+Add the following custom target to the [Directory.Solution.targets](https://learn.microsoft.com/en-us/visualstudio/msbuild/customize-solution-build?view=vs-2022) file to be able to invoke analysis of the whole solution:
+
+```xml
+<Project>
+
+    <ItemGroup>
+        <ProjectsToAnalyze Include="src\**\*.fsproj" />
+    </ItemGroup>
+
+    <Target Name="AnalyzeSolution">
+        <Exec Command="dotnet build -c Release $(SolutionFileName)" />
+        <MSBuild
+                Projects="@(ProjectsToAnalyze)"
+                Targets="AnalyzeProject"
+                Properties="DesignTimeBuild=True;Configuration=Release;ProvideCommandLineArgs=True;SkipCompilerExecution=True" />
+    </Target>
+
+</Project>
+```
+
+Finally, add the following custom target to the [Directory.Build.targets](https://learn.microsoft.com/en-us/visualstudio/msbuild/customize-by-directory?view=vs-2022) file. This takes care of invoking analysis of single projects:
+```xml
+<Project>
+
+    <Target
+        Name="AnalyzeProject" 
+        DependsOnTargets="Restore;ResolveAssemblyReferencesDesignTime;ResolveProjectReferencesDesignTime;ResolvePackageDependenciesDesignTime;FindReferenceAssembliesForReferences;_GenerateCompileDependencyCache;_ComputeNonExistentFileProperty;BeforeBuild;BeforeCompile;CoreCompile">
+        
+        <Message Importance="normal" Text="fsc arguments: @(FscCommandLineArgs)" />
+        <Message Importance="High" Text="Analyzing $(MSBuildProjectFile)"/>
+        <Exec
+            ContinueOnError="true"
+            Command="dotnet fsharp-analyzers --fsc-args &quot;@(FscCommandLineArgs)&quot; --analyzers-path &quot;$(PkgG-Research_FSharp_Analyzers)\analyzers\dotnet\fs&quot; --exclude-analyzer PartialAppAnalyzer --fail-on-warnings GRA-STRING-001 GRA-STRING-002 GRA-STRING-003 GRA-UNIONCASE-001 --verbose --report &quot;$(MSBuildProjectName)-analysis.sarif&quot;">
+            <Output TaskParameter="ExitCode" PropertyName="LastExitCode" />
+        </Exec>
+        <Error Condition="'$(LastExitCode)' == '-2'" Text="Problems were found $(MSBuildProjectFile)" />
+
+    </Target>
+
+</Project>
+```
+
+You may need to adjust the `Command` to be compatible with your specific analyzer. Think about how you want warnings to be treated.
+
+⚠️ Note: This uses a feature from .NET 8 to get hold of the `FscCommandLineArgs`.
+
+At last, you can run the analyzer from the solution folder:
+
+```shell
+dotnet msbuild /t:AnalyzeSolution
+```
 *)
+
 (**
 
 [Next]({{fsdocs-next-page-link}})
