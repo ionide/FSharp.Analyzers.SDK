4b4027476205bd76cd8fd0f2de0e32dd8e09dd28
dawe <dawedawe@posteo.de>
2023-10-27 10:32:16 +0200

    - rename "Getting Started.fsx" to "Getting Started Using.fsx" - only use --project based project cracking for now - various deeper explainations

--- docs/content/Getting Started Using.fsx
+++ docs/content/Getting Started Using.fsx
@@ -18,7 +18,7 @@ First, we need to add the [fsharp-analyzers](https://www.nuget.org/packages/fsha
 dotnet tool install fsharp-analyzers
 ```
 
-Next, add the `PackageReference` pointing to your favorite analyzers to the `.fsproj` file of the project you want to analyzse:
+Next, add the `PackageReference` pointing to your favorite analyzers to the `.fsproj` file of the project you want to analyze:
 
 ```xml
 <PackageReference Include="G-Research.FSharp.Analyzers" Version="0.1.6">
@@ -27,24 +27,26 @@ Next, add the `PackageReference` pointing to your favorite analyzers to the `.fs
 </PackageReference>
 ```
 
+At the time of writing, the [G-Research analyzers](https://github.com/g-research/fsharp-analyzers) [package](https://www.nuget.org/packages/G-Research.FSharp.Analyzers) contains the only analyzers compatible with the latest CLI tool.  
 Finally, add a custom MSBuild target to the `.fsproj` file for easy invocation of the analyzer:
 
 ```xml
-<Target
-    Name="AnalyzeProject" 
-    DependsOnTargets="Restore;ResolveAssemblyReferencesDesignTime;ResolveProjectReferencesDesignTime;ResolvePackageDependenciesDesignTime;FindReferenceAssembliesForReferences;_GenerateCompileDependencyCache;_ComputeNonExistentFileProperty;BeforeBuild;BeforeCompile;CoreCompile">
+<Target Name="AnalyzeProject">
 
     <Message Importance="High" Text="Analyzing $(MSBuildProjectFile)"/>
     <Exec
         ContinueOnError="true"
-        Command="dotnet fsharp-analyzers --project &quot;$(MSBuildProjectFile)&quot; --analyzers-path &quot;$(PkgG-Research_FSharp_Analyzers)\analyzers\dotnet\fs&quot; --exclude-analyzer PartialAppAnalyzer --fail-on-warnings GRA-STRING-001 GRA-STRING-002 GRA-STRING-003 GRA-UNIONCASE-001 --verbose --report &quot;$(MSBuildProjectName)-analysis.sarif&quot;">
+        Command="dotnet fsharp-analyzers --project &quot;$(MSBuildProjectFile)&quot; --analyzers-path &quot;$(PkgG-Research_FSharp_Analyzers)\analyzers\dotnet\fs&quot; --exclude-analyzer PartialAppAnalyzer --fail-on-warnings GRA-STRING-001 --verbose --report &quot;$(MSBuildProjectName)-analysis.sarif&quot;">
         <Output TaskParameter="ExitCode" PropertyName="LastExitCode" />
     </Exec>
     <Error Condition="'$(LastExitCode)' == '-2'" Text="Problems were found $(MSBuildProjectFile)" />
 </Target>
 ```
 
-You may need to adjust the `Command` to be compatible with your specific analyzer. Think about how you want warnings to be treated.
+You may need to adjust the `Command` to be compatible with your specific analyzer. Think about how you want warnings to be treated.  
+To locate the analyzer DLLs in the filesystem, we use the variable `$(PkgG-Research_FSharp_Analyzers)`. It's produced by NuGet and normalized to be usable by [MSBuild](https://learn.microsoft.com/en-us/nuget/consume-packages/package-references-in-project-files#generatepathproperty).
+In general, a `Pkg` prefix is added and dots in the package ID are replaced by underscores. But make sure to look at the [nuget.g.props](https://learn.microsoft.com/en-us/nuget/reference/msbuild-targets#restore-outputs) file in the `obj` folder for the exact string.  
+The `\analyzers\dotnet\fs` subpath is a convention analyzer authors should follow when creating their packages.
 
 At last, you can run the analyzer from the project folder:
 
@@ -90,19 +92,19 @@ Add the following custom target to the [Directory.Solution.targets](https://lear
 </Project>
 ```
 
-Finally, add the following custom target to the [Directory.Build.targets](https://learn.microsoft.com/en-us/visualstudio/msbuild/customize-by-directory?view=vs-2022) file. This takes care of invoking analysis of single projects:
+Finally, add the following custom target to the [Directory.Build.targets](https://learn.microsoft.com/en-us/visualstudio/msbuild/customize-by-directory?view=vs-2022) file.
+ This takes care of invoking analysis of single projects:
 ```xml
 <Project>
 
     <Target
-        Name="AnalyzeProject" 
-        DependsOnTargets="Restore;ResolveAssemblyReferencesDesignTime;ResolveProjectReferencesDesignTime;ResolvePackageDependenciesDesignTime;FindReferenceAssembliesForReferences;_GenerateCompileDependencyCache;_ComputeNonExistentFileProperty;BeforeBuild;BeforeCompile;CoreCompile">
+        Name="AnalyzeProject">
         
         <Message Importance="normal" Text="fsc arguments: @(FscCommandLineArgs)" />
         <Message Importance="High" Text="Analyzing $(MSBuildProjectFile)"/>
         <Exec
             ContinueOnError="true"
-            Command="dotnet fsharp-analyzers --fsc-args &quot;@(FscCommandLineArgs)&quot; --analyzers-path &quot;$(PkgG-Research_FSharp_Analyzers)\analyzers\dotnet\fs&quot; --exclude-analyzer PartialAppAnalyzer --fail-on-warnings GRA-STRING-001 GRA-STRING-002 GRA-STRING-003 GRA-UNIONCASE-001 --verbose --report &quot;$(MSBuildProjectName)-analysis.sarif&quot;">
+            Command="dotnet fsharp-analyzers --project &quot;$(MSBuildProjectFile)&quot; --analyzers-path &quot;$(PkgG-Research_FSharp_Analyzers)\analyzers\dotnet\fs&quot; --exclude-analyzer PartialAppAnalyzer --fail-on-warnings GRA-STRING-001 GRA-STRING-002 GRA-STRING-003 GRA-UNIONCASE-001 --verbose --report &quot;$(MSBuildProjectName)-analysis.sarif&quot;">
             <Output TaskParameter="ExitCode" PropertyName="LastExitCode" />
         </Exec>
         <Error Condition="'$(LastExitCode)' == '-2'" Text="Problems were found $(MSBuildProjectFile)" />
@@ -112,15 +114,21 @@ Finally, add the following custom target to the [Directory.Build.targets](https:
 </Project>
 ```
 
+This is effectively the same as adding a target to each `*.proj` file which exist in subfolders.
 You may need to adjust the `Command` to be compatible with your specific analyzer. Think about how you want warnings to be treated.
-
-⚠️ Note: This uses a feature from .NET 8 to get hold of the `FscCommandLineArgs`.
 
 At last, you can run the analyzer from the solution folder:
 
 ```shell
 dotnet msbuild /t:AnalyzeSolution
 ```
+
+## Project Cracking
+
+If all this seems a bit complex to you, let us explain some inner details to give you a better understanding:  
+The way the analyzers work is that we will programmatically type-check a project and process the results with our analyzers. In order to do this programmatically we need to construct the [FSharpProjectOptions](https://fsharp.github.io/fsharp-compiler-docs/reference/fsharp-compiler-codeanalysis-fsharpprojectoptions.html).  
+This is essentially a type that represents all the fsharp compiler arguments. When using --project, we will use [ProjInfo](https://github.com/ionide/proj-info) to invoke a set of MSBuild targets in the project to perform a design-time build.  
+A design-time build is basically an empty invocation of a build. It won't produce assemblies but will have constructed the correct arguments to theoretically invoke the compiler.
 *)
 
 (**
